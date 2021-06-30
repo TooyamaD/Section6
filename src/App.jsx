b4c07cc6964344f58abe24cbeb74ad5b8a2d0594
TooyamaD <77679561+TooyamaD@users.noreply.github.com>
2021-06-30 12:48:16 +0900

    追記 追記

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./Components/ColorfulMEssage";
 
 const App = () => {
-  const onClickButton = () => alert();
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const [num, setNum] = useState(0);
 
   return (
     <>
       {/* 　javaScriptでstyleを当てる。HTMLの中にjavaScriptを入れるためには{} その中の｛｝はオブジェクト */}
       <h1 style={{ color: "red" }}>こんにちは!</h1>
-
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です</ColorfulMessage>
 
       {/* {}を入れるとそこにはjavaScriptが反映 */}
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
