90ddbf2f5a857019298d52527ec08b1895895984
TooyamaD <77679561+TooyamaD@users.noreply.github.com>
2021-08-28 09:04:06 +0900

    useEffect追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ColorfulMessage from "./Components/ColorfulMEssage";
 
 const App = () => {
+  console.log("最初");
+  const [num, setNum] = useState(0);
+  const [flag, setFlag] = useState(false);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+  const onClickFlag = () => {
+    setFlag(!flag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        flag || setFlag(true);
+      } else {
+        flag && setFlag(false);
+      }
+    }
+  }, [num]);
 
   return (
     <>
@@ -16,7 +32,10 @@ const App = () => {
 
       {/* {}を入れるとそこにはjavaScriptが反映 */}
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickFlag}>on/off</button>
       <p>{num}</p>
+      {flag && <p>(^_-)-☆</p>}
     </>
   );
 };
